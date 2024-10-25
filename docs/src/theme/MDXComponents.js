06b2ee9c0ffacfaa1f21965b2e6fa4de2ac65e3a
efe-pullflow <efe@pullflow.com>
2024-10-25 16:35:07 -0700

    newstuff

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -5,5 +5,6 @@ export default {
   // Re-use the default mapping.
   // See https://docusaurus.io/docs/markdown-features/react#mdx-component-scope
   ...MDXComponents,
+  ...json.ext,
   ShowForTs,
 }
