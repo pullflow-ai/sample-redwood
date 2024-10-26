314b98c8ab7e5f2c258c37b6b5e4baf13a001f33
Efe Torunoglu <efe@pullflow.com>
2024-10-25 19:48:01 -0700

    Update index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -2,11 +2,6 @@ import React from 'react'
 
 import { Redirect, useLocation } from '@docusaurus/router'
 
-import config from '../../docusaurus.config'
-
-const { defaultDocsLandingPage } = config.customFields
-
-function Home() {
   const location = useLocation()
   const defaultUrl = [
     location.pathname.replace(/\/$/, ''),
